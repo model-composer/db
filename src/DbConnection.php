195e094f0746c8c8e1f4a6d3f17bfbc046e752d9
Domenico Giambra <zorinik@gmail.com>
2022-05-25 23:35:20 +0200

    Tables caching

--- src/DbConnection.php
+++ src/DbConnection.php
@@ -1,5 +1,6 @@
 <?php namespace Model\Db;
 
+use Model\Cache\Cache;
 use Model\DbParser\Parser;
 use Model\QueryBuilder\QueryBuilder;
 
@@ -9,6 +10,8 @@ class DbConnection
 	private \PDO $db;
 	private Parser $parser;
 	private QueryBuilder $builder;
+
+	private array $inMemoryCache = [];
 
 	private int $c_transactions = 0;
 	private array $query_counters = [
@@ -123,6 +126,10 @@ class DbConnection
 	 */
 	public function selectAll(string $table, array|int $where = [], array $options = []): iterable
 	{
+		$cacheable = $this->isSelectCacheable($table, $where, $options);
+		if ($cacheable)
+			return $this->selectFromCache($table, $where, $options);
+
 		$qry = $this->builder->select($table, $where, $options);
 		if ($options['debug'] ?? false)
 			echo "QUERY: " . $qry . "\n";
@@ -140,6 +147,115 @@ class DbConnection
 
 			return $resultsArr;
 		}
+	}
+
+	/**
+	 * @param string $table
+	 * @param array|int $where
+	 * @param array $options
+	 * @return bool
+	 */
+	private function isSelectCacheable(string $table, array|int $where = [], array $options = []): bool
+	{
+		// Only full selects or selects by id are cacheable
+		if (is_array($where) and count($where) > 0)
+			return false;
+
+		// If I did request select not to be cached, I return false
+		if (!($options['cache'] ?? true))
+			return false;
+
+		// Only simple queries can be cached
+		if (!empty($options['joins']) or !empty($options['fields']) or !empty($options['group_by']))
+			return false;
+
+		// If order_by is used, only array form is cacheable
+		if (($options['order_by'] ?? null) and !is_array($options['order_by']))
+			return false;
+
+		// If limit is used, only normal limits are accepted
+		if (($options['limit'] ?? null) and !preg_match('/^[0-9]+(, ?[0-9+])?$/', $options['limit']))
+			return false;
+
+		// Only tables with no more than 200 rows are cached
+		if ($this->count($table) > 200)
+			return false;
+
+		return true;
+	}
+
+	/**
+	 * @param string $table
+	 * @param array|int $where
+	 * @param array $options
+	 * @return array
+	 */
+	private function selectFromCache(string $table, array|int $where = [], array $options = []): array
+	{
+		$rows = $this->getItemFromCache($table, 'model.db.cache.tables.' . $table . '.rows', function (\Symfony\Contracts\Cache\ItemInterface $item) use ($table) {
+			$item->tag(['db.cache.tables', 'db.cache.tables.' . $table]);
+			$item->expiresAfter(3600 * 24);
+
+			Cache::registerInvalidation('tag', ['db.cache.tables']);
+
+			$tableModel = $this->parser->getTable($table);
+
+			$response = [];
+			$results = $this->selectAll($table, [], ['cache' => false]);
+			foreach ($results as $row)
+				$response[$row[$tableModel->primary[0]]] = $row;
+
+			return $response;
+		});
+
+		// Select by id
+		if (is_int($where))
+			return isset($rows[$where]) ? [$rows[$where]] : [];
+
+		if ($options['order_by'] ?? null) {
+			usort($rows, function ($a, $b) use ($options) {
+				foreach ($options['order_by'] as $sortingField) {
+					if (is_string($sortingField))
+						$sortingField = [$sortingField, 'ASC'];
+
+					if ($a[$sortingField[0]] != $b[$sortingField[0]])
+						return $a[$sortingField[0]] <=> $b[$sortingField[0]];
+				}
+
+				return 0;
+			});
+		} else {
+			$rows = array_values($rows);
+		}
+
+		if ($options['limit'] ?? null) {
+			if (is_numeric($options['limit'])) {
+				$rows = array_slice($rows, 0, $options['limit']);
+			} else {
+				$limit = explode(',', $options['limit']);
+				$rows = array_slice($rows, $limit[0], $limit[1]);
+			}
+		}
+
+		return $rows;
+	}
+
+	/**
+	 * Middleware method that stores in memory repeated cache queries
+	 *
+	 * @param string $table
+	 * @param string $key
+	 * @param callable $getter
+	 * @return mixed
+	 */
+	private function getItemFromCache(string $table, string $key, callable $getter): mixed
+	{
+		if (!isset($this->inMemoryCache[$table], $this->inMemoryCache[$table][$key])) {
+			$cache = Cache::getCacheAdapter();
+			$this->inMemoryCache[$table][$key] = $cache->get($key, $getter);
+		}
+
+		return $this->inMemoryCache[$table][$key];
 	}
 
 	/**
@@ -167,8 +283,13 @@ class DbConnection
 
 		$qry = implode(' UNION ', $qry_str);
 
-		if (($options['order_by'] ?? null) !== null)
+		if (($options['order_by'] ?? null) !== null) {
+			if (!is_string($options['order_by']))
+				throw new \Exception('Currently, only strings "order by" are supported in union selects');
+
 			$qry .= ' ORDER BY ' . $options['order_by'];
+		}
+
 		if (($options['limit'] ?? null) !== null)
 			$qry .= ' LIMIT ' . $options['limit'];
 
@@ -258,6 +379,15 @@ class DbConnection
 	 */
 	public function count(string $table, array $where = [], array $options = []): int
 	{
+		if (empty($where) and empty($options)) {
+			// Simple counts are cached
+			return (int)$this->getItemFromCache($table, 'model.db.cache.tables.' . $table . '.count', function (\Symfony\Contracts\Cache\ItemInterface $item) use ($table) {
+				$item->tag(['db.cache.tables', 'db.cache.tables.' . $table]);
+				$item->expiresAfter(3600 * 24);
+				return $this->count($table, [], ['cache' => false]);
+			});
+		}
+
 		$options['fields'] = 'COUNT(*)';
 		$qry = $this->builder->select($table, $where, $options);
 		if ($options['debug'] ?? false)
@@ -307,7 +437,25 @@ class DbConnection
 			}
 		}
 
+		// Cache invalidation for that specific table
+		if ($table and $type !== 'SELECT')
+			$this->tableChanged($table);
+
 		return $this->db->query($query);
+	}
+
+	/**
+	 * Invalidates cache for a specific table
+	 *
+	 * @param string $table
+	 * @return void
+	 */
+	private function tableChanged(string $table): void
+	{
+		$cache = Cache::getCacheAdapter();
+		$cache->invalidateTags(['model.db.cache.tables.' . $table]);
+		if (isset($this->inMemoryCache[$table]))
+			unset($this->inMemoryCache[$table]);
 	}
 
 	/**
