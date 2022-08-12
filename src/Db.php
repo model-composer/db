8879c3d5ed8485407b3754c71a24ca5361ff22e8
Domenico Giambra <zorinik@gmail.com>
2022-08-12 19:36:47 +0200

    Basic migrations logic

--- src/Db.php
+++ src/Db.php
@@ -1,6 +1,8 @@
 <?php namespace Model\Db;
 
 use Model\Config\Config;
+use Phinx\Console\PhinxApplication;
+use Phinx\Wrapper\TextWrapper;
 
 class Db
 {
@@ -35,6 +37,78 @@ class Db
 	}
 
 	/**
+	 * @param string $db
+	 * @param array $dbConfig
+	 * @return void
+	 */
+	public static function injectDatabase(string $db, array $dbConfig): void
+	{
+		$config = self::getConfig();
+		if (isset($config['databases'][$db]))
+			throw new \Exception('Db ' . $db . ' already existing');
+
+		$config['databases'][$db] = $dbConfig;
+		Config::set('db', $config);
+	}
+
+	/**
+	 * Executes migrations
+	 *
+	 * @return void
+	 */
+	public static function migrate(): void
+	{
+		$config = self::getConfig();
+		foreach ($config['databases'] as $databaseName => $database) {
+			if (!$database['migrations_folder'])
+				continue;
+
+			$appRoot = self::getProjectRoot();
+			if (!is_dir($appRoot . $database['migrations_folder']))
+				mkdir($appRoot . $database['migrations_folder']);
+
+			$phinxConfig = [
+				'paths' => [
+					'migrations' => $appRoot . '/migrations',
+				],
+				'environments' => [
+					"production" => [
+						'adapter' => 'mysql',
+						'host' => $database['host'],
+						'port' => $database['port'],
+						'name' => $database['name'],
+						'user' => $database['username'],
+						'pass' => $database['password'],
+						'charset' => 'utf8',
+					],
+				],
+			];
+
+			$phinxConfigFile = $appRoot . $database['migrations_folder'] . DIRECTORY_SEPARATOR . 'tmp_config_' . $databaseName . '.php';
+			file_put_contents($phinxConfigFile, "<?php\nreturn " . var_export($phinxConfig, true) . ";\n");
+
+			$app = new PhinxApplication();
+			$wrap = new TextWrapper($app);
+			$wrap->setOption('configuration', $phinxConfigFile);
+			$err = $wrap->getMigrate();
+			if ($wrap->getExitCode() !== 0)
+				throw new \Exception('Errors while migrating: ' . $err);
+
+			unlink($phinxConfigFile);
+		}
+	}
+
+	/**
+	 * Retrieves project root
+	 *
+	 * @return string
+	 */
+	private static function getProjectRoot(): string
+	{
+		return realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR;
+	}
+
+	/**
 	 * Config retriever
 	 *
 	 * @return array
@@ -60,6 +134,15 @@ class Db
 							],
 						],
 					];
+				},
+			],
+			[
+				'version' => '0.3.0',
+				'migration' => function (array $config, string $env) {
+					foreach ($config['databases'] as &$database)
+						$database['migrations_folder'] = 'migrations';
+
+					return $config;
 				},
 			],
 		]);
